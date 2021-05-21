5f53ad464830e4bdada85e31a2a2e8a1725c0816
Gajendra-Goswami <79796567+Gajendra-Goswami@users.noreply.github.com>
2021-05-21 12:13:24 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Class definition for text web form
-
-Created on Mon Apr 24 11:44:44 2017
-
-@author: Mashimo
-"""
-
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, BooleanField
 from wtforms.validators import DataRequired
